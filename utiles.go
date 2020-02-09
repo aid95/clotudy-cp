070fd8ee5344e408bd83a34d273da731d6de39d3
kjuno95 <admin@mail.pppr.io>
2020-02-10 02:53:38 +0900

    명령행 수행을 위한 유틸리티 함수 작성

--- utiles.go
+++ utiles.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"crypto/md5"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 )
 
 // GetSystemEnv 환경변수값을 가져오는데 없다면 기본값 반환
@@ -30,4 +32,14 @@ func Md5HashGen(plaintxt string) string {
 	h := md5.New()
 	io.WriteString(h, plaintxt)
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func RunCommandLine(cmdline string, arg []string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(cmdline, arg...)
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
 }
